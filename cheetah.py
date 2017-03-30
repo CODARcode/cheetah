b60b282908215f87ff714c594aa4ab41c77224c4
Bryce Allen <bryce@bda.ath.cx>
2017-03-30 15:58:29 -0500

    tweak pi example and parser

--- cheetah.py
+++ cheetah.py
@@ -13,28 +13,41 @@ def test_parse(experiment_spec_path, out_dir):
         data = yaml.load(f)
     print(data)
     e = Experiment(data)
-    print(e.parameter_sets)
-    e.generate_execute_dir(out_dir)
+    for cmd in e.get_commands():
+        print(cmd)
 
 
-def parameter_set_to_cross_product_iter(data):
+def parameter_group_to_cross_product_iter(data):
     """
-    Convert a parameter set specifiation defining the values that each
+    Convert a parameter group specifiation defining the values that each
     parameter can take (as a list or range spec) to a cross product of
     the parameter values as a list of dicts.
     """
-    params = sorted(data.keys())
-    param_value_lists = [parse_parameter_list(data[k]) for k in params]
-    return (params, itertools.product(*param_value_lists))
+    param_names = sorted(data.keys())
+    param_value_lists = [parse_parameter_list(data[k]) for k in param_names]
+    return [dict(zip(param_names, values))
+            for values in itertools.product(*param_value_lists)]
 
 
 def parse_parameter_list(data):
+    """
+    Given a dict containing 'range-start', 'range-end', and optionally
+    one of 'range-increment' or 'range-multiplier', produce an iterator of
+    values from start to end _inclusive_ using the specified increment or
+    multiplier. Defaults to an increment of 1 if not specified.
+
+    Given a list, return as is.
+    """
     if isinstance(data, dict):
         # range specification
         try:
             start = data['range-start']
             end = data['range-end']
             if 'range-increment' in data:
+                if 'range_multiplier' in data:
+                    raise ValueError(
+                        'specify one of range-multiplier or range-increment, '
+                        'not both')
                 return range(start, end+1, data['range-increment'])
             elif 'range-multiplier' in data:
                 multiplier = data['range-multiplier']
@@ -55,62 +68,89 @@ def parse_parameter_list(data):
         raise ValueError('Expected list or dict of range-* keys')
 
 
-def get_run_command(runner, app_script, run_params, default_params, outpath):
+class AbstractParameterLayer(object):
     """
-    Given the runner, e.g. 'aprun', the app script, e.g. 'my-app.sh', the
-    parameters for this particular run, default parameters, and the result
-    dir for this run, produce a string suitable for insertion into a scheduler
-    script.
+    Base class for Scheduler and Runner, both of which have a type and
+    parameters.
     """
-    params = default_params
-    params.update(run_params)
-    runner_command = runner
-    app_command = app_script + ' --output-directory="' + outpath + '"'
-    for k, v in params.items():
-        v = str(v)
-        if k.startswith('runner-'):
-            k = k[len('runner-'):]
-            # TODO: quoting
-            app_command += ' -' + k + ' ' + v
-        elif k.startswith('app-'):
-            # TODO: quoting
+    TYPES = [] # subclasses must override
+
+    def __init__(self, data):
+        try:
+            self.type = data['type']
+            if self.type not in self.TYPES:
+                raise ValueError('Unknown type: %s' % self.type)
+            self.parameters = data.get('parameters', {})
+        except KeyError as e:
+            raise ValueError('Missing required key in scheduler data: %s' % e)
+
+
+class Scheduler(AbstractParameterLayer):
+    TYPES = ['pbs']
+
+
+class Runner(AbstractParameterLayer):
+    TYPES = ['aprun']
+
+
+class App(object):
+    def __init__(self, data):
+        try:
+            self.script = data['script']
+            self.max_walltime = data['max-walltime']
+        except KeyError as e:
+            raise ValueError('Missing required key in app data: %s' % e)
+
+
+class RunParameters(object):
+    """
+    Parameters to run a single instance of the application.
+    """
+    def __init__(self, data):
+        self.app = {}
+        self.runner = {}
+        for k, v in data.items():
+            if k.startswith('app-'):
+                k = k[len('app-'):]
+                self.app[k] = v
+            elif k.startswith('runner-'):
+                k = k[len('runner-'):]
+                self.runner[k] = v
+            else:
+                raise ValueError('parameter group keys must start with "app-"'
+                                 ' or "runner-" (got "%s")' % k)
+
+    def get_command(self, runner, app_script):
+        app_command = app_script
+        runner_command = runner
+        for k, v in self.app.items():
+            v = str(v)
             app_command += ' --' + k + '=' + v
-    return runner_command + ' ' + app_command
+        for k, v in self.runner.items():
+            v = str(v)
+            # TODO: quoting
+            runner_command += ' -' + k + ' ' + v
+        return runner_command + ' ' + app_command
 
 
 class Experiment(object):
     def __init__(self, data):
         self._data = data
-        self.experiment = data.get('experiment')
-        self.app_script = self.experiment['app']['script']
-        if self.experiment is None:
-            raise ValueError('Missing top level experiment key')
-        self.scheduler = self.experiment.get('scheduler')
-        if self.scheduler is None:
-            raise ValueError('Missing scheduler key in experiment')
-        self.runner = self.scheduler['runner']
-        self.runner_defaults = self.scheduler['runner-defaults']
-        self.scheduler_type = self.scheduler['type']
-        self.parameter_sets = []
-        for pset in self.experiment['parameter-sets']:
-            self.parameter_sets.append(
-                parameter_set_to_cross_product_iter(pset))
-
-    def generate_execute_dir(self, outdir):
         try:
-            os.mkdir(outdir)
-        except OSError as e:
-            if e.errno != errno.EEXIST:
-                raise
-        i = 1
-        for pset_keys, pset_iter in self.parameter_sets:
-            for param_tuple in pset_iter:
-                run_resultdir = os.path.join(outdir, 'results-%03d' % i)
-                params = dict(zip(pset_keys, param_tuple))
-                cmd = get_run_command(self.runner, self.app_script, params,
-                                      self.runner_defaults, run_resultdir)
-                print(cmd)
-                i += 1
+            self.experiment = data['experiment']
+            self.app = App(self.experiment['app'])
+            self.scheduler = Scheduler(self.experiment['scheduler'])
+            self.runner = Runner(self.experiment['runner'])
+            self.run_parameters_list = []
+            for pgroup in self.experiment['parameter-groups']:
+                for pdict in parameter_group_to_cross_product_iter(pgroup):
+                    self.run_parameters_list.append(RunParameters(pdict))
+        except KeyError as e:
+            raise ValueError('Missing required key in experiment: %s' % e)
+
+    def get_commands(self):
+        return [rp.get_command(self.runner.type, self.app.script)
+                for rp in self.run_parameters_list]
 
 
 if __name__ == '__main__':
