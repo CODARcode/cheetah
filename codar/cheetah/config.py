d065c0d6a3fb5731f282ac78d1700ede93626eb5
Bryce Allen <bdallen@uchicago.edu>
2017-06-30 14:07:33 -0500

    fix previous commit missing file

---
+++ codar/cheetah/config.py
@@ -0,0 +1,20 @@
+"""
+Cheetah paths and (in future) features for loading site configuration.
+"""
+import os.path
+
+CHEETAH_PATH = os.path.realpath(os.path.join(
+                     os.path.dirname(__file__), "..", ".."))
+
+CHEETAH_PATH_SCRIPTS = os.path.join(CHEETAH_PATH, "scripts")
+
+CHEETAH_PATH_MACHINE_CONFIG = os.path.join(CHEETAH_PATH, "machine_config")
+
+
+def script_path(script_name):
+    return os.path.join(CHEETAH_PATH_SCRIPTS, script_name)
+
+
+def machine_submit_env_path(machine_name):
+    return os.path.join(CHEETAH_PATH_MACHINE_CONFIG, machine_name,
+                        'submit-env.sh')
