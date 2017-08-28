2d2fb9b4c7610224ae9a5c1490ee98beb975d339
Bryce Allen <bdallen@uchicago.edu>
2017-08-28 16:25:46 -0500

    add second pipeline to local script test

--- tests/test-local-scripts.sh
+++ tests/test-local-scripts.sh
@@ -3,16 +3,18 @@
 cd "$(dirname $0)"/..
 SRC_DIR=$(pwd)
 OUT_DIR="$SRC_DIR"/test_output/local_scripts
-RUN_DIR="$OUT_DIR"/group/run
+RUN_DIR1="$OUT_DIR"/group/run1
+RUN_DIR2="$OUT_DIR"/group/run2
 
 mkdir -p "$OUT_DIR"
 rm -rf "$OUT_DIR"/*
-mkdir -p "$RUN_DIR"
+mkdir -p "$RUN_DIR1"
+mkdir -p "$RUN_DIR2"
 
 # Copy local experiment scripts to output directory
 cp -R "$SRC_DIR"/scripts/local/* "$OUT_DIR"
 
-# Set up a fake environment for the campaign and group
+# Set up an environment for the campaign and group
 cat >"$OUT_DIR"/campaign-env.sh <<EOF
 export CODAR_CHEETAH_EXPERIMENT_DIR="$OUT_DIR"
 export CODAR_CHEETAH_MACHINE_CONFIG=""
@@ -21,16 +23,16 @@ export CODAR_WORKFLOW_RUNNER="none"
 export CODAR_CHEETAH_WORKFLOW_LOG_LEVEL="DEBUG"
 EOF
 
-# Set up a fake environment for the experiment
 cat >"$OUT_DIR"/group/group-env.sh <<EOF
-export CODAR_CHEETAH_GROUP_WALLTIME="10"
+export CODAR_CHEETAH_GROUP_WALLTIME="30"
 export CODAR_CHEETAH_GROUP_MAX_PROCS="2"
 EOF
 
 
 # Create synthetic fobs
 cat >"$OUT_DIR"/group/fobs.json <<EOF
-[{ "name": "echo1", "exe": "/usr/bin/echo", "args": ["one"], "nprocs": 1, "working_dir": "$RUN_DIR"}, { "name": "echo2", "exe": "/usr/bin/echo", "args": ["two"], "nprocs": 1, "working_dir": "$RUN_DIR"}]
+[{ "name": "echo1", "exe": "/usr/bin/echo", "args": ["one"], "nprocs": 1, "working_dir": "$RUN_DIR1"}, { "name": "echo2", "exe": "/usr/bin/echo", "args": ["two"], "nprocs": 1, "working_dir": "$RUN_DIR1"}]
+[{ "name": "echo1", "exe": "/usr/bin/echo", "args": ["one-b"], "nprocs": 1, "working_dir": "$RUN_DIR2"}, { "name": "echo2", "exe": "/usr/bin/echo", "args": ["two-b"], "nprocs": 1, "working_dir": "$RUN_DIR2"}]
 EOF
 
 # Run
