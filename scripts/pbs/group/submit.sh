65803b5567981e432accc9676cfdf5e08ef1b960
Bryce Allen <bdallen@uchicago.edu>
2017-09-25 15:44:31 -0500

    check qsub result, fixes #31

--- scripts/pbs/group/submit.sh
+++ scripts/pbs/group/submit.sh
@@ -13,12 +13,22 @@ fi
 secs=$CODAR_CHEETAH_GROUP_WALLTIME
 PBS_WALLTIME=$(printf '%02d:%02d:%02d\n' $(($secs/3600)) $(($secs%3600/60)) $(($secs%60)))
 
-JOBID=$(qsub \
+OUTPUT=$(qsub \
         -A $CODAR_CHEETAH_SCHEDULER_ACCOUNT \
         -q $CODAR_CHEETAH_SCHEDULER_QUEUE \
         -N "$CODAR_CHEETAH_CAMPAIGN_NAME-$CODAR_CHEETAH_GROUP_NAME" \
         -l nodes=$CODAR_CHEETAH_GROUP_NODES \
         -l walltime=$PBS_WALLTIME \
         run-group.pbs)
+
+rval=$?
+
+if [ $rval != 0 ]; then
+    echo "SUBMIT FAILED:"
+    echo $OUTPUT
+    exit $rval
+fi
+
+JOBID=$OUTPUT
 
 echo "PBS:$JOBID" > codar.cheetah.jobid.txt
