726547b611c14ba383e2acab9f6dcb81a0e35731
Mehta, Kshitij V <kshitij-v-mehta@users.noreply.github.com>
2017-10-24 10:57:52 -0400

    Added a new spec file using heat-transfer to demonstrate how to specify that each component needs to run in its own subdir inside the run dir, and how to specify per-component input files.
    This is a temporary file, useful for demo and testing. Can use cheetah to create the campaign endpoint, but do not run the campaign, it will fail.
    Waiting for a spec file from xgc to show these features.

--- tests/test-examples.sh
+++ tests/test-examples.sh
@@ -31,6 +31,27 @@ rm -rf test_output/local-heat-simple/*
     -a "$CODAR_APPDIR/Example-Heat_Transfer/" \
     -o test_output/local-heat-simple
 
+
+# ------------- per component subdirs and inputs -----------
+
+# Example to demonstrate running components in separate subdirs
+# and specifying inputs files per component.
+# Ok to generate campaign for testing, but will not run.
+# Temporary example only, waiting for xgc spec file.
+
+rm -rf test_output/local-heat-rc-subdirs-inputs/*
+./cheetah.py -e examples/heat_transfer_simple_rc_subdirs_inputs.py -m local \
+    -a "$CODAR_APPDIR/Example-Heat_Transfer/" \
+    -o test_output/local-heat-simple-rc-subdirs-inputs
+
+rm -rf test_output/titan-heat-rc-subdirs-inputs/*
+./cheetah.py -e examples/heat_transfer_simple_rc_subdirs_inputs.py -m titan \
+    -a "$CODAR_APPDIR/Example-Heat_Transfer/" \
+    -o test_output/titan-heat-simple-rc-subdirs-inputs
+
+# End ------------- per component subdirs and inputs -----------
+
+
 rm -rf test_output/titan-heat-simple/*
 ./cheetah.py -e examples/heat_transfer_simple.py -m titan \
     -a "$CODAR_APPDIR/Example-Heat_Transfer/" \
