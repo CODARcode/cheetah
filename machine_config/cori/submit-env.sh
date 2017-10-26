87ce609827a441eac1cfe4e9d7790838abad5ee6
Bryce Allen <bdallen@uchicago.edu>
2017-10-26 14:26:00 -0500

    don't assume spack is installed on cori
    TODO: require user provided setup script? Eventually maybe we
    can provide pre-built savanna modules, but things are changing too
    fast now.

--- machine_config/cori/submit-env.sh
+++ machine_config/cori/submit-env.sh
@@ -2,6 +2,5 @@
 
 source $MODULESHOME/init/bash
 
-# savanna packages pre-installed on cori
 module load python/3.6-anaconda-4.4 cray-mpich/7.6.0
-spack load adios@1.12.0 flexpath@1.12
+#spack load adios@1.12.0 flexpath@1.12
