2f385cf812d6a950c8a1a755480b57ff5993ace9
Mehta, Kshitij V <kshitij-v-mehta@users.noreply.github.com>
2017-12-15 23:14:38 -0500

    work in progress

--- codar/cheetah/analyze.py
+++ codar/cheetah/analyze.py
@@ -2,14 +2,44 @@
 Functions to analyze an already completed campaign.
 Parses all run directories in all sweep groups to aggregate information.
 Runs sosflow analysis to collect data.
+
+All parameters specified in the spec file must be used as column headers in
+an output csv file.
 """
 
 from pathlib import Path
 import json
 from glob import glob
+import csv
 
 
-def parse_run_dir(run_dir):
+def __serialize_params_nested_dict(nested_run_params_dict):
+    """
+    codar.cheetah.run-params.json has the structure:
+    {
+        app1: {
+            param1: value1
+            param2: value2
+        }
+        app2: {
+            param1: value1
+            param2: value2
+        }
+    }
+
+    Serialize this structure so that we have
+    {app1__param1: value1, app1__param2:value2, and so on}.
+    """
+    serialized_dict = {}
+    for key in nested_run_params_dict:
+        for nested_key in nested_run_params_dict[key]:
+            new_key = key + "__" + nested_key
+            serialized_dict[new_key] = nested_run_params_dict[key][nested_key]
+
+    return serialized_dict
+
+
+def __parse_run_dir(run_dir, parsed_runs, unique_keys):
     """
     Parse run directory of a sweep group
     """
@@ -57,13 +87,18 @@ def parse_run_dir(run_dir):
               ". Skipping " + run_dir)
         return
 
-    print(run_params_dict)
-    exit(0)
+    # Serialize nested dict and add to list of parsed run dicts
+    serialized_run_params = __serialize_params_nested_dict(run_params_dict)
+    parsed_runs.append(serialized_run_params)
+
+    # Add any new params discovered in this run dir to unique keys
+    for key in serialized_run_params:
+        unique_keys.add(key)
 
     # Run sosflow analysis on the run_dir now.
 
 
-def parse_sweep_group(sweep_group):
+def __parse_sweep_group(sweep_group, parsed_runs, unique_keys):
     """
     Parse sweep group and get post-run performance information
     """
@@ -93,8 +128,7 @@ def parse_sweep_group(sweep_group):
 
     # Parse runs that have succeeded
     for run_dir in successful_runs:
-        parse_run_dir(run_dir)
-    exit(0)
+        __parse_run_dir(run_dir, parsed_runs, unique_keys)
 
 
 def analyze():
@@ -103,7 +137,14 @@ def analyze():
     It walks the campaign tree and retrieves performance information
     about all completed runs.
     """
-
+    
+    # A list of dicts. Each dict contains metadata and performance information
+    # about the run.
+    parsed_runs = []
+    
+    # Unique application parameters that can be used as headers for csv output
+    unique_keys = set()
+    
     # This should only be run in the campaign top-level directory.
     # Verify that current dir is the campaign endpoint
     # by checking for the presence of the campaign-env.sh file.
@@ -118,7 +159,13 @@ def analyze():
         return
 
     for sweep_group in sweep_groups:
-        parse_sweep_group("./" + sweep_group)
+        __parse_sweep_group("./" + sweep_group, parsed_runs, unique_keys)
+
+    # Write the parsed results to csv
+    with open('campaign_results.csv', 'w') as f:
+        dict_writer = csv.DictWriter(f, sorted(unique_keys))
+        dict_writer.writeheader()
+        dict_writer.writerows(parsed_runs)
 
 
 if __name__ == "__main__":
