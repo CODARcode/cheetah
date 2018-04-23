d33694544eeba75cde9f59c8f7b4a3b938d4ab35
Kshitij Mehta <kshitij-v-mehta@users.noreply.github.com>
2018-04-23 09:42:09 -0400

    updated script to a new version provided by kevin huck. adding more information to the output dict - memory information , adios and mpi exclusive/inclusive timers etc. still has temp hack to resolve unicode decode error.

--- codar/cheetah/sos_flow_analysis.py
+++ codar/cheetah/sos_flow_analysis.py
@@ -109,7 +109,6 @@ def get_ranks(c):
     prog_ranks = np.array([x[1] for x in all_rows])
     high_res = np.chararray((len(prog_names)))
     high_res[:] = 'off'
-    #high_res = 'off'
     return allranks[0], prog_names, prog_ranks, high_res
 
 # What was the total time for each application?
@@ -147,9 +146,15 @@ def get_group_metric(c,timer_type,metric,group,prog_name):
     return np.sum(alltime)
 
 # How many bytes was sent in <group>?
-def get_group_counter(c,group,prog_name,high_res):
+def get_group_counter(c,group,prog_name,high_res,stat):
     if high_res == 'off':
         sql_statement = "select cast(value as decimal), value_name, comm_rank, max(frame) from viewCombined where value_name like '%Total:" + group + "' and prog_name = '" + prog_name + "' group by value_name, comm_rank;"
+        try_execute(c,sql_statement);
+        all_rows = c.fetchall()
+        bytes = np.array([x[0] for x in all_rows]).astype(np.float)
+        sum = np.sum(bytes)
+    elif stat != 'Total':
+        sql_statement = "select cast(value as decimal), value_name, comm_rank, max(frame) from viewCombined where value_name like '%" + stat + ":" + group + "' and prog_name = '" + prog_name + "' group by value_name, comm_rank;"
         try_execute(c,sql_statement);
         all_rows = c.fetchall()
         bytes = np.array([x[0] for x in all_rows]).astype(np.float)
@@ -184,13 +189,12 @@ def sos_flow_analysis(run_dir):
     #make_view(c)
     
     total_ranks,prog_names,prog_ranks,high_res = get_ranks(c)
-    #print("Total ranks in allocation :", total_ranks, "\n")
+    print("Total ranks in allocation :", total_ranks, "\n")
     zipped = zip(prog_names, prog_ranks, high_res)
     for n,r,h in zip(prog_names, prog_ranks, high_res):
         n_str = n.decode("utf-8")
         #h_str = h.decode("utf-8")
-        
-        # Kshitij: override
+        #h_str = unicode(h, errors='replace')
         h_str = "on"
         #print("Ranks in", n_str, ":", r)
         if h_str == "off":
@@ -198,46 +202,65 @@ def sos_flow_analysis(run_dir):
             user_exclusive = (get_group_metric(c,"exclusive","TIME","TAU_USER",n_str)/1000000)/r
             mpi_exclusive = (get_group_metric(c,"exclusive","TIME","MPI",n_str)/1000000)/r
             adios_exclusive = (get_group_metric(c,"exclusive","TIME","TAU_IO",n_str)/1000000)/r
-            mpi_collective_bytes = get_group_counter(c,"Collective Bytes Sent",n_str,h_str)
-            mpi_recv_bytes = get_group_counter(c,"MPI Receive Bytes",n_str,h_str)
-            mpi_send_bytes = get_group_counter(c,"MPI Send Bytes",n_str,h_str)
-            io_read_bytes = get_group_counter(c,"IO Bytes Read",n_str,h_str)
-            io_write_bytes = get_group_counter(c,"IO Bytes Written",n_str,h_str)
-            adios_write_bytes = get_group_counter(c,"ADIOS data size",n_str,h_str)
+            mpi_collective_bytes = get_group_counter(c,"Collective Bytes Sent",n_str,h_str,"Total")
+            mpi_recv_bytes = get_group_counter(c,"MPI Receive Bytes",n_str,h_str,"Total")
+            mpi_send_bytes = get_group_counter(c,"MPI Send Bytes",n_str,h_str,"Total")
+            io_read_bytes = get_group_counter(c,"IO Bytes Read",n_str,h_str,"Total")
+            io_write_bytes = get_group_counter(c,"IO Bytes Written",n_str,h_str,"Total")
+            adios_write_bytes = get_group_counter(c,"ADIOS data size",n_str,h_str,"Total")
         else:
             total_inclusive = (get_group_metric(c,"inclusive","TIME","TAU_USER:.TAU application",n_str)/1000000)/r
-            mpi_exclusive = (get_group_metric(c,"exclusive","TIME","MPI:MPI_",n_str)/1000000)/r
+            mpi_exclusive = (get_group_metric(c,"inclusive","TIME","MPI:MPI_",n_str)/1000000)/r
             adios_exclusive = (get_group_metric(c,"exclusive","TIME","TAU_IO:adios_",n_str)/1000000)/r
+            adios_inclusive = (get_group_metric(c,"inclusive","TIME","TAU_IO:adios_",n_str)/1000000)/r
             user_exclusive = total_inclusive - (mpi_exclusive + adios_exclusive)
-            mpi_collective_bytes = get_group_counter(c,"Message size for %",n_str,h_str)
-            mpi_recv_bytes = get_group_counter(c,"Message size received from all nodes",n_str,h_str)
-            mpi_send_bytes = get_group_counter(c,"Message size sent to all nodes",n_str,h_str)
-            io_read_bytes = get_group_counter(c,"Bytes Read",n_str,h_str)
-            io_write_bytes = get_group_counter(c,"Bytes Written",n_str,h_str)
-            adios_write_bytes = get_group_counter(c,"ADIOS data size",n_str,h_str)
-        #if True:
-        #    print("\t","Total time :",total_inclusive,"seconds")
-        #    print("\t","User  time :",user_exclusive,"seconds")
-        #    print("\t","MPI   time :",mpi_exclusive,"seconds (",(mpi_exclusive/total_inclusive)*100,"% )")
-        #    print("\t","ADIOS time :",adios_exclusive,"seconds (",(adios_exclusive/total_inclusive)*100,"% )")
-        #    print("\t","MPI collective bytes   :",mpi_collective_bytes)
-        #    print("\t","MPI P2P bytes sent     :",mpi_send_bytes)
-        #    print("\t","MPI P2P bytes received :",mpi_recv_bytes)
-        #    print("\t","Posix IO bytes read    :",io_read_bytes)
-        #    print("\t","Posix IO bytes written :",io_write_bytes)
-        #    print("\t","ADIOS bytes written    :",adios_write_bytes)
-        #    print("")
+            mpi_collective_bytes = get_group_counter(c,"Message size for %",n_str,h_str,"Total")
+            mpi_recv_bytes = get_group_counter(c,"Message size received from all nodes",n_str,h_str,"Total")
+            mpi_send_bytes = get_group_counter(c,"Message size sent to all nodes",n_str,h_str,"Total")
+            io_read_bytes = get_group_counter(c,"Bytes Read",n_str,h_str,"Total")
+            io_write_bytes = get_group_counter(c,"Bytes Written",n_str,h_str,"Total")
+            adios_write_bytes = get_group_counter(c,"ADIOS data size",n_str,h_str,"Total")
+            high_water_mark = get_group_counter(c,"Peak Memory Usage Resident Set Size (VmHWM) (KB)",n_str,h_str,"Max")
+            resident_set_size = get_group_counter(c,"Memory Footprint (VmRSS) (KB)",n_str,h_str,"Mean")
+        if True:
+            print("\t","Total time :",total_inclusive,"seconds")
+            print("\t","User  time :",user_exclusive,"seconds")
+            print("\t","MPI   time :",mpi_exclusive,"seconds (",(mpi_exclusive/total_inclusive)*100,"% )")
+            print("\t","ADIOS time :",adios_exclusive,"seconds (",(adios_exclusive/total_inclusive)*100,"%, excludes MPI)")
+            print("\t","ADIOS time :",adios_inclusive,"seconds (",(adios_inclusive/total_inclusive)*100,"%, includes MPI)")
+            print("\t","MPI collective bytes             :",mpi_collective_bytes)
+            print("\t","MPI P2P bytes sent               :",mpi_send_bytes)
+            print("\t","MPI P2P bytes received           :",mpi_recv_bytes)
+            print("\t","Posix IO bytes read              :",io_read_bytes)
+            print("\t","Posix IO bytes written           :",io_write_bytes)
+            print("\t","ADIOS bytes written              :",adios_write_bytes)
+            print("\t","Max Memory Footprint (HWM) (kB)  :",high_water_mark)
+            print("\t","Mean Memory Footprint (RSS) (kB) :",resident_set_size)
+            print("")
         
         rc_exe = n_str
         if "/" in n_str:
             rc_exe = n_str.split("/")[-1]
-        perf_results[rc_exe] = {"time": total_inclusive,
-                                "mpi_time": mpi_exclusive,
-                               "adios_time": adios_exclusive,
-                               "adios_data": adios_write_bytes}
-        #print perf_results
-        return(perf_results)
+        perf_results[rc_exe] = {"time": round(total_inclusive,2),
+                               "adios_time": round(adios_exclusive,2),
+                               "adios_bytes_written": round(adios_write_bytes,2),
+                               "mpi_exclusive_time" : round(mpi_exclusive,2),
+                               "adios_exclusive_time": round(adios_exclusive,2),
+                               "adios_inclusive_time": round(adios_inclusive,2),
+                               "max_mem_footprint_KB": round(high_water_mark,2),
+                               "mean_mem_footprint_KB": round(resident_set_size,2)}
+    print (perf_results)
+    return(perf_results)
+
+if __name__ == '__main__':
+    #sos_flow_analysis("/lustre/atlas/world-shared/csc143/kmehta/heat-transfer")
+    if len(sys.argv) == 2:
+        sos_flow_analysis(sys.argv[1])
+    elif len(sys.argv) == 1:
+        sos_flow_analysis(os.getcwd())
+
 
 if __name__ == '__main__':
     pr = sos_flow_analysis(sys.argv[1])
-    print (pr)
+    #print (pr)
+
