ed1161d5923fdcbaed0caf46b4f3f1b51a32a813
Bryce Allen <bdallen@uchicago.edu>
2018-06-28 09:06:26 -0500

    update version in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='cheetah',
-      version='0.2a',
+      version='0.5',
       description='CODAR Experiment Harness',
       url='https://github.com/CODARcode/cheetah',
       packages=['codar.cheetah', 'codar.workflow'],
