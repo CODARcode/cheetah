34a96cd314549c97a4e45a8bca1d4ed63bef3076
Bryce Allen <bdallen@uchicago.edu>
2018-09-28 15:17:15 -0500

    refactor for setuptools install
    - Move templates and config files into cheetah package data, for easier
      inclusion into a distribution
    - Move driver scripts cheetah.py and workflow.py into bin and install
      with scripts setuptools option
    - Move old scripts into tests/scripts
    - Use pkgutil-style namespace package fofr codar
    - Update example script to work with new layout, at least in develop
      mode (pip install -e .)

--- codar/cheetah/config.py
+++ codar/cheetah/config.py
@@ -5,18 +5,22 @@ import os.path
 import math
 from codar.cheetah import exc
 
+PACKAGE_PATH = os.path.realpath(os.path.dirname(__file__))
+DATA_PATH = os.path.join(PACKAGE_PATH, "data")
+
 CHEETAH_PATH = os.path.realpath(os.path.join(
                      os.path.dirname(__file__), "..", ".."))
 
-CHEETAH_PATH_SCRIPTS = os.path.join(CHEETAH_PATH, "scripts")
+CHEETAH_PATH_SCHEDULER = os.path.join(DATA_PATH, "scheduler")
 
-CHEETAH_PATH_MACHINE_CONFIG = os.path.join(CHEETAH_PATH, "machine_config")
+CHEETAH_PATH_MACHINE_CONFIG = os.path.join(DATA_PATH, "machine_config")
 
-WORKFLOW_SCRIPT = os.path.join(CHEETAH_PATH, "workflow.py")
+# TODO: this is not robust
+WORKFLOW_SCRIPT = os.path.join(CHEETAH_PATH, "bin", "workflow.py")
 
 
-def script_path(script_name):
-    return os.path.join(CHEETAH_PATH_SCRIPTS, script_name)
+def scheduler_path(scheduler_name):
+    return os.path.join(CHEETAH_PATH_SCRIPTS, scheduler_name)
 
 
 def machine_submit_env_path(machine_name):
@@ -25,7 +29,7 @@ def machine_submit_env_path(machine_name):
 
 
 def etc_path(conf_name):
-    return os.path.join(CHEETAH_PATH, "etc", conf_name)
+    return os.path.join(DATA_PATH, "etc", conf_name)
 
 
 def get_dataspaces_num_servers(num_dimes_clients, num_dataspaces_clients):
