df4fe5fc50fca411a2acb51f3bdee06d0983d3da
igor <igor@igor-XPS-15-9550>
2019-05-31 10:17:50 -0500

    split zchecker.cpp

--- examples/gray-scott-zchecker/analysis/update.sh
+++ examples/gray-scott-zchecker/analysis/update.sh
@@ -1,1 +1,1 @@
-rsync -av /home/igor/tmp3/adiosvm/Tutorial/gray-scott/analysis/{zchecker.cpp,Makefile} /home/igor/test1/cheetah/examples/gray-scott-zchecker/analysis/
+rsync -av /home/igor/tmp3/adiosvm/Tutorial/gray-scott/analysis/{zchecker.cpp,zchecker.h,main.cpp,Makefile} /home/igor/test1/cheetah/examples/gray-scott-zchecker/analysis/
