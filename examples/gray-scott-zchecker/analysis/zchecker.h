0b2a4b2f3eff182facf3bfdfcee79caa1706bb8f
igor <igor@igor-XPS-15-9550>
2019-05-31 18:42:23 -0500

    ftk output

--- examples/gray-scott-zchecker/analysis/zchecker.h
+++ examples/gray-scott-zchecker/analysis/zchecker.h
@@ -24,5 +24,4 @@ void z_check_mgard(int stepAnalysis, std::vector<double>& u, const std::string &
 void z_check_zfp(int stepAnalysis, std::vector<double>& u, const std::string &solution);
 void z_check_sz(int stepAnalysis, std::vector<double>& u, const std::string &solution,
 		const std::vector<std::size_t>& shape);
-void extract_features(double *data, size_t nx, size_t, size_t nz);
 #endif
