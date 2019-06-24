97055f5799a37bf2adf6680c30ff077f2d0f0cfe
igor <igor@igor-XPS-15-9550>
2019-06-24 15:11:17 -0500

    ftk features save

--- examples/gray-scott-zchecker/env.sh
+++ examples/gray-scott-zchecker/env.sh
@@ -1,7 +1,9 @@
-module load adios2/051719git
+#module load adios2/051719git
+#module load adios2/062019git
+module load adios2/062319git
 module load z-checker
 module load ftk/git
-module load paraview/5.6.0
+#module load paraview/5.6.0
 
 source /home/igor/test1/cheetah/venv-cheetah/bin/activate
 
