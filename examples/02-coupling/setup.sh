3da385a0e3a4c4c2d91e9c5d3b50baf720462b7f
Kshitij Mehta <kshitij-v-mehta@users.noreply.github.com>
2019-06-25 13:35:29 -0400

    1] Created numbered directories for examples. exmaples/01-eulers_number and so on. 2] Added a Python-based example that shows a workflow coupled using ADIOS

---
+++ examples/02-coupling/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export PYTHONPATH=path-to-cheetah-root:$PYTHONPATH
+export LD_LIBRARY_PATH=path-to-adios-lib:$LD_LIBRARY_PATH
+
---
+++ examples/03-brusselator/setup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export LD_LIBRARY_PATH=~/vshare/opt/ADIOS2-master-01.13.19/installation/lib/:$LD_LIBRARY_PATH
+
