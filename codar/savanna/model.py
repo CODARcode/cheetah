39bd4920e4715570054fbfa9200dbe0fe5fcef18
Kshitij Mehta <kshitij-v-mehta@users.noreply.github.com>
2020-07-20 00:08:38 -0400

    Fixes #200. Now checking if args is None before running filter on it.

--- codar/savanna/model.py
+++ codar/savanna/model.py
@@ -78,7 +78,8 @@ class Run(threading.Thread):
         threading.Thread.__init__(self, name="Thread-Run-" + name)
         self.name = name
         self.exe = exe
-        self.args = list(filter(None, args))
+        self.args = args
+        if args: self.args = list(filter(None, args))
         self.sched_args = sched_args
         self.env = env or {}
         self.working_dir = working_dir
