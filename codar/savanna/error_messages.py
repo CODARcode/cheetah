c3b207d2650828f39b775de96cdaa5620ea04325
Kshitij Mehta <kshitij-v-mehta@github.com>
2020-09-09 11:01:00 -0400

    Fixes mpmd file issue for Summit #201

--- codar/savanna/error_messages.py
+++ codar/savanna/error_messages.py
@@ -1,11 +1,16 @@
 err_msg = dict()
 
 err_msg['mpmd_run_error'] = \
-    "Internal error. Expected MPMD Run type, but the run does not contain " \
+    "ERROR: Internal error. Expected MPMD Run type, but the run does not " \
+    "contain " \
     "child runs"
 
 err_msg['run_node_config_missing'] = \
-    "Internal error. Node config not found in run object."
+    "ERROR: Internal error. Node config not found in run object."
 
 err_msg['rc_env_out_fail'] = \
-    "Could not store the environment for {} to {}. Ignoring .."
+    "ERROR: Could not store the environment for {} to {}. Ignoring .."
+
+err_msg['f_creat'] = \
+    "ERROR: Could not create file {}."
+
