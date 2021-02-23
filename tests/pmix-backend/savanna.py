b0a750af6dc7db4dcbba4fa4fdf3dc28a9e294e6
Kshitij Mehta <kshitij-v-mehta@users.noreply.github.com>
2021-02-23 12:44:54 -0500

    updates

--- tests/pmix-backend/savanna.py
+++ tests/pmix-backend/savanna.py
@@ -11,10 +11,9 @@ for PMIx is created.
 import pdb
 import math
 import os
-import signal
-import subprocess
 import threading
-import summit_helper
+from jsrun_backend import JsrunExecutor
+import pmix_backend
 
 
 #----------------------------------------------------------------------------#
@@ -61,53 +60,6 @@ class PmixExecutor():
 
 
 #----------------------------------------------------------------------------#
-class JsrunExecutor():
-    """Jsrun executor for an application on Summit
-    """
-    def __init__(self, run):
-        self.run = run
-        self.erf_file = "{}.erf_input".format(self.run.name)
-        self.pid = None
-
-    def execute(self):
-        self._assign_nodes()
-        summit_helper.create_erf_file(self.erf_file, self.run.name,
-            self.run.working_dir, self.run.exe, self.run.args, self.run.nprocs,
-            self.run.nodes, self.run.nodes_assigned, self.run.node_config)
-        self._submit()
-        self._wait()
-    
-    def _assign_nodes(self):
-        """Assign hostids to the Run.
-        These are relative indices, thus they always start from 1.
-        """
-        assert self.run.nodes > 0
-        self.run.nodes_assigned = []
-        for i in range(self.run.nodes):
-            self.run.nodes_assigned.append(i+1)
-
-    def _submit(self):
-        """Submit a jsrun using Popen
-        """
-        submit_cmd = "jsrun --erf_input {}".format(self.erf_file)
-        popen_args = list(submit_cmd.split())
-        self.pid = subprocess.Popen(popen_args, cwd=self.run.working_dir)
-
-    def _wait(self):
-        """Wait for a running Popen instance to complete
-        """
-        try:
-            self.pid.wait(self.run.timeout)
-        except subprocess.TimeoutExpired:
-            self._kill()
-    
-    def _kill(self):
-        """Kill a Run that seems to have timedout.
-        """
-        os.killpg(os.getpgid(self.pid.pid, signal.SIGTERM))
-
-
-#----------------------------------------------------------------------------#
 class Run(threading.Thread):
     """Represents an application instance
     """
@@ -127,10 +79,10 @@ class Run(threading.Thread):
         self.timeout = 60
 
         # Redirect stdout and stderr
-        stdout_path = "{}.stdout".format(self.name)
-        stderr_path = "{}.stderr".format(self.name)
-        self.args.extend(['>', stdout_path])
-        self.args.extend(['2>', stderr_path])
+        # stdout_path = "{}.stdout".format(self.name)
+        # stderr_path = "{}.stderr".format(self.name)
+        # self.args.extend(['>', stdout_path])
+        # self.args.extend(['2>', stderr_path])
 
         # Set the backend here
         self._backend_executor = JsrunExecutor(self)
